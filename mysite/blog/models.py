2b62458117869b5ef0ef31ddfe386c870f8f6700
stephenv13 <stephenvogelmeier@gmail.com>
2022-05-01 10:06:40 -0400

    updated models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,49 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
 
-# new comment
+# create blog post
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    # publish blog post
+    def publish(self) -> None:
+        self.published_date = timezone.now()
+        self.save()
+
+    # approve comment on blog post
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+    
+    # after making blog post, go to url of the new blog post
+    def get_absolute_url(self):
+        return reverse('post_detail',kwargs={'pk':self.pk})
+    
+    # str representation of blog post
+    def __str__(self) -> str:
+        return self.title
+
+# create comment
+class Comments(model.Model):
+    post = models.ForeignKey('blog.post',related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approved(self) -> None:
+        self.approved_comment = True
+        self.save()
+
+    # once user is done writing comment, return to main page
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self) -> str:
+        return self.text
